967f633e68b73f9960e1fcf936c82f751b981b3d
Kaire <sepp.kaire2002@gmail.com>
2023-02-07 23:32:40 +0200

    SOCIAL-17 renamed the colors && got rid of the repeats

--- client/src/components/main/navigation.tsx
+++ client/src/components/main/navigation.tsx
@@ -1,42 +1,48 @@
 import "./navigation.css"
-export default function Navigation() { // &nbsp; adds a gap between the buttons
+export default function Navigation() { 
     return (
         <header className={"header"}>
-            <nav className={"navigation"}>  
-                <input 
-                    type={"button"} 
-                    className={"navigation__logout"}
-                    value={"Logout"}
-                    onClick={() => {window.location.assign("/")}}
-                />
-                <input 
-                    type={"button"} 
-                    className={"navigation__users"}
-                    value={"Users"}
-                    onClick={() => {window.location.assign("#/users")}}
-                />
-                &nbsp;
-                <input 
-                    type={"button"} 
-                    className={"navigation__groups"}
-                    value={"Groups"}
-                    onClick={() => {window.location.assign("#/groups")}}
-                />
-                &nbsp;
-                <input 
-                    type={"button"} 
-                    className={"navigation__notifications"}
-                    value={"Notifications"}
-                    onClick={() => {window.location.assign("#/notifications")}}
-                />
-                &nbsp; 
-                <input 
-                    type={"button"} 
-                    className={"navigation__profile"}
-                    value={"Profile"}
-                    onClick={() => {window.location.assign("#/profile")}}
-                />
+            <nav className={"navigation"}> 
+                <nav className={"navigation-left"}>
+                    <input 
+                        type={"button"} 
+                        className={"navigation__profile"}
+                        value={"Profile"}
+                        onClick={() => {window.location.assign("#/profile")}}
+                    />
+
+                    <input 
+                        type={"button"} 
+                        className={"navigation__groups"}
+                        value={"Groups"}
+                        onClick={() => {window.location.assign("#/groups")}}
+                    />
+
+                    <input 
+                        type={"button"} 
+                        className={"navigation__users"}
+                        value={"Users"}
+                        onClick={() => {window.location.assign("#/users")}}
+                    />
+                    
+                    <input 
+                        type={"button"} 
+                        className={"navigation__notifications"}
+                        value={"Notifications"}
+                        onClick={() => {window.location.assign("#/notifications")}}
+                    />
+                </nav> 
+                <nav className={"navigation-right"}>
+                    <input
+                        type={"button"} 
+                        className={"navigation__logout"}
+                        value={"Logout"}
+                        onClick={() => {window.location.assign("/")}}
+                    />
             </nav>
+              
+            </nav>
+
         </header>
     );
 }
