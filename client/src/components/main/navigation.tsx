2bb22f2c3aeabc503d6dac1c740eba4ed8221e6a
mercey123 <erik1telpt2@gmail.com>
2023-02-08 16:18:01 +0300

    SOCIAL-17 fixes + removed unnecessary parts

--- client/src/components/main/navigation.tsx
+++ client/src/components/main/navigation.tsx
@@ -1,48 +1,35 @@
 import "./navigation.css"
-export default function Navigation() { 
+export default function Navigation() {
     return (
-        <header className={"header"}>
-            <nav className={"navigation"}> 
-                <nav className={"navigation-left"}>
-                    <input 
-                        type={"button"} 
-                        className={"navigation__profile"}
-                        value={"Profile"}
-                        onClick={() => {window.location.assign("#/profile")}}
-                    />
-
-                    <input 
-                        type={"button"} 
-                        className={"navigation__groups"}
-                        value={"Groups"}
-                        onClick={() => {window.location.assign("#/groups")}}
-                    />
-
-                    <input 
-                        type={"button"} 
-                        className={"navigation__users"}
-                        value={"Users"}
-                        onClick={() => {window.location.assign("#/users")}}
-                    />
-                    
-                    <input 
-                        type={"button"} 
-                        className={"navigation__notifications"}
-                        value={"Notifications"}
-                        onClick={() => {window.location.assign("#/notifications")}}
-                    />
-                </nav> 
-                <nav className={"navigation-right"}>
-                    <input
-                        type={"button"} 
-                        className={"navigation__logout"}
-                        value={"Logout"}
-                        onClick={() => {window.location.assign("/")}}
-                    />
+        <header className="header">
+            <nav className="navigation-left">
+                <input
+                    type="button"
+                    className="navigation__button"
+                    value="Profile"
+                />
+                <input
+                    type="button"
+                    className="navigation__button"
+                    value="Groups"
+                />
+                <input
+                    type="button"
+                    className="navigation__button"
+                    value="Users"
+                />
+                <input
+                    type="button"
+                    className="navigation__button"
+                    value="Notifications"
+                />
             </nav>
-              
-            </nav>
-
+            <input
+                type="button"
+                className="navigation__button navigation__button_red"
+                value="Logout"
+            // onClick={logout}
+            />
         </header>
     );
 }
