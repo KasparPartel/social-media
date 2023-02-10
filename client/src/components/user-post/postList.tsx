ed52a77cb408fd06526d9cd0fc9b0f21e245ec49
Kaspar Johannes Pärtel <kasparjohannes@gmail.com>
2023-02-10 16:27:14 +0200

    SOCIAL-27 some logic still needs to be implemented

--- client/src/components/user-post/postList.tsx
+++ client/src/components/user-post/postList.tsx
@@ -1,75 +1,39 @@
 import UserPost from "./userPost"
-import { Post } from "./userPost"
 
-const postList: Post[] = [
-    {
-        text:
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and..." +
-            "On the other hand, we denounce with righteous indignation and dislike men who are" +
-            " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
-            " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and...",
+// const postList: Post[] = [
+//     {
+//         text:
+//             "On the other hand, we denounce with righteous indignation and dislike men who are" +
+//             " so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they" +
+//             " cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and...",
+//         attachments: ["https://via.placeholder.com/150"],
+//     },
+//     {
+//         text: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
+//         attachments: ["https://via.placeholder.com/150", "https://via.placeholder.com/150"],
+//     },
+//     {
+//         text: "",
+//         attachments: [
+//             "https://via.placeholder.com/150",
+//             "https://via.placeholder.com/150",
+//             "https://via.placeholder.com/150",
+//             "https://via.placeholder.com/900",
+//         ],
+//     },
+//     {
+//         text: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
+//         attachments: [],
+//     },
+// ]
 
-        attachments: ["https://via.placeholder.com/150"],
-    },
-    {
-        text: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
-        attachments: ["https://via.placeholder.com/150", "https://via.placeholder.com/150"],
-    },
-    {
-        text: "",
-        attachments: [
-            "https://via.placeholder.com/150",
-            "https://via.placeholder.com/324",
-            "https://via.placeholder.com/645",
-            "https://via.placeholder.com/90",
-            "https://via.placeholder.com/900",
-            "https://via.placeholder.com/150",
-            "https://via.placeholder.com/10",
-        ],
-    },
-    {
-        text: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
-        attachments: [],
-    },
-]
+const postIdList: number[] = [1, 2, 3]
 
 export default function PostList() {
     return (
         <div className="postList">
-            {postList.map((post, i) => (
-                <UserPost post={post} key={i} />
+            {postIdList.map((postId, i) => (
+                <UserPost postId={postId} key={i} />
             ))}
         </div>
     )
--- client/src/components/user-post/userPost.tsx
+++ client/src/components/user-post/userPost.tsx
@@ -1,73 +1,97 @@
 import "./userPost.css"
 import icon from "../../assets/SVGRepo_iconCarrier.svg"
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 
 export interface Post {
+    id: number
+    userId: number
     text: string | null
     attachments: string[]
 }
 
 interface UserPostProps {
-    post: Post
+    postId: number
 }
 
-export default function UserPost({ post }: UserPostProps) {
-    const [attachmentsOpen, setAttachmentsOpen] = useState<boolean>(false)
-    const attachmentsCount = post.attachments.length
-    const [textOpen, setTextOpen] = useState<boolean>(false)
+export default function UserPost({ postId }: UserPostProps) {
+    const [post, setPost] = useState<Post>()
+    const [err, setErr] = useState<Error | null>(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [attachmentsOpen, setAttachmentsOpen] = useState(false)
+    const [attachmentsCount, setAttachmentsCount] = useState(0)
+
+    useEffect(() => {
+        setTimeout(
+            () =>
+                fetch(`http://localhost:8080/post/${postId}`)
+                    .then((res) => {
+                        if (res.status >= 400) {
+                            throw new Error("Server responds with error!")
+                        }
+                        return res.json()
+                    })
+                    .then(
+                        (data) => {
+                            setPost(data)
+                            setIsLoaded(true)
+                        },
+                        (err) => {
+                            setErr(err)
+                            setIsLoaded(true)
+                        },
+                    ),
+            3000,
+        )
+    }, [])
+
+    useEffect(() => {
+        if (post) {
+            setAttachmentsCount(post.attachments.length)
+        }
+    }, [post])
 
     const toggleAttachments = () => {
         setAttachmentsOpen(!attachmentsOpen)
     }
 
-    const togglePostText = () => {
-        setTextOpen(!textOpen)
-    }
-
-    const refText = useRef<HTMLDivElement>(null)
-    const [height, setHeight] = useState<number>(215)
-    useEffect(() => {
-        textOpen ? setHeight(refText.current.scrollHeight) : setHeight(215)
-    }, [textOpen])
-
-    return (
-        <article className="post">
-            {post.text ? (
-                <p
-                    ref={refText}
-                    className="post__text"
-                    style={{ maxHeight: `${height}px` }}
-                    onClick={() => {
-                        togglePostText()
-                    }}
-                >
-                    {post.text}
-                </p>
-            ) : (
-                <img className="post__attachment" src={post.attachments[0]} alt="placeholder" />
-            )}
-
-            {attachmentsCount > 0 &&
-                attachmentsOpen &&
-                post.attachments
-                    .slice(post.text ? 0 : 1)
-                    .map((att, i) => (
-                        <img className="post__attachment" key={i} src={att} alt="placeholder" />
-                    ))}
-
-            <div className="post__actions">
-                {attachmentsCount > 0 && (
-                    <p className="post__attachments-toggler" onClick={() => toggleAttachments()}>
-                        {attachmentsOpen
-                            ? "Show less"
-                            : `Show ${
-                                  post.text ? attachmentsCount : attachmentsCount - 1
-                              } more attachment${attachmentsCount > 1 ? "s" : ""}`}
-                    </p>
+    if (err) {
+        return <div>{err.message}</div>
+    } else if (!isLoaded) {
+        return <div>Loading...</div>
+    } else {
+        return (
+            <article className="post">
+                {post.text ? (
+                    <p className="post__text">{post.text}</p>
+                ) : (
+                    <img className="post__attachment" src={post.attachments[0]} alt="placeholder" />
                 )}
 
-                <img className="post__add-comment" src={icon} alt="comment icon" />
-            </div>
-        </article>
-    )
+                {attachmentsCount > 0 &&
+                    attachmentsOpen &&
+                    post.attachments
+                        .slice(post.text ? 0 : 1)
+                        .map((att, i) => (
+                            <img className="post__attachment" key={i} src={att} alt="placeholder" />
+                        ))}
+
+                <div className="post__actions">
+                    {attachmentsCount > 0 && (
+                        <p
+                            className="post__attachments-toggler"
+                            onClick={() => toggleAttachments()}
+                        >
+                            {attachmentsOpen
+                                ? "Show less"
+                                : `Show ${
+                                      post.text ? attachmentsCount : attachmentsCount - 1
+                                  } more attachment${attachmentsCount > 1 ? "s" : ""}`}
+                        </p>
+                    )}
+
+                    <img className="post__add-comment" src={icon} alt="comment icon" />
+                </div>
+            </article>
+        )
+    }
 }
