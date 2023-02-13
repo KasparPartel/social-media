59ac16900b76bbe37a4c83ceb5ac5e52ee20a0cc
mercey123 <erik1telpt2@gmail.com>
2023-02-13 17:39:46 +0300

    SOCIAL-20 test data moved out to variable

--- client/src/components/user-information/user-information.tsx
+++ client/src/components/user-information/user-information.tsx
@@ -1,8 +1,18 @@
 import { useEffect, useRef, useState } from "react";
+// import { User } from "../models";
 import "./user-information.css"
 
 export interface UserInfoProps {
-    isMyProfile: boolean
+    isMyProfile: boolean,
+    // userData: User
+}
+
+const testData = {
+    name: "Name Lastname",
+    email: "profile@email.name",
+    login: "profile_login",
+    aboutMe: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.",
+    birthDate: "xx.xx.xxx"
 }
 
 export function UserInfo({ isMyProfile }: UserInfoProps) {
@@ -25,7 +35,7 @@ export function UserInfo({ isMyProfile }: UserInfoProps) {
                     <div className="short-info">
                         <div className="short-info__avatar" />
                         <div className="short-info__name">
-                            Name Lastname
+                            {testData.name}
                         </div>
                         <button
                             className="button"
@@ -43,10 +53,10 @@ export function UserInfo({ isMyProfile }: UserInfoProps) {
                     <div
                         ref={refText}
                         className="detailed-info">
-                        <p className="detailed-info__section">profile@email.name</p>
-                        <p className="detailed-info__section">profile_login</p>
-                        <p className="detailed-info__section">On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.</p>
-                        <p className="detailed-info__section">xx.xx.xxx</p>
+                        <p className="detailed-info__section">{testData.email}</p>
+                        <p className="detailed-info__section">{testData.login}</p>
+                        <p className="detailed-info__section">{testData.aboutMe}</p>
+                        <p className="detailed-info__section">{testData.birthDate}</p>
                     </div>
                 </div>
             </div>
