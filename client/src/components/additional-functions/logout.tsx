5ed2f8701c7c85f78a58134eed7f221ceb3d722f
mercey123 <erik1telpt2@gmail.com>
2023-02-16 02:08:57 +0300

    SOCIAL-19 removed console log

--- client/src/components/additional-functions/logout.tsx
+++ client/src/components/additional-functions/logout.tsx
@@ -3,7 +3,6 @@ import { fetchHandler } from "./fetchHandler"
 export function Logout(navigate: (path: string) => void) {
     fetchHandler(`http://localhost:8080/logout`, "POST")
         .then((r: Response) => {
-            console.log(r)
             if (r.status === 200) {
                 navigate("/login")
                 return
