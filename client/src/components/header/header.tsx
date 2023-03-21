4c257c237baaabac0256bb6529409767e8ce37cd
Kaire <sepp.kaire2002@gmail.com>
2023-03-21 21:18:19 +0200

    SOCIAL-18 remade the button functions

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,28 +1,57 @@
 import "./header.css"
 import { Logout } from "../../additional-functions/logout"
-import { useNavigate} from "react-router-dom"
-import { disable } from "../../additional-functions/disableButton"
+import { Outlet, useNavigate} from "react-router-dom"
+import { useState } from "react";
 
 export default function Navigation() {
     const navigate = useNavigate();
     const id = localStorage.getItem("id")
+    const [button, setButton] = useState<boolean[]>([false, false, false, false])
 
 
     return (
+        <>
         <header className="header">
             <nav className="navigation-left">
-                <input 
-                    type="button"
-                    className="button navigation__button" 
-                    value="Profile"
-                    onClick={() => {
-                        navigate(`/user/${id}`);
-                        disable(0);
-                    }}
-                />
-                <input type="button" className="button navigation__button" value="Groups" />
-                <input type="button" className="button navigation__button" value="Users" />
-                <input type="button" className="button navigation__button" value="Notifications" />
+              {Button(
+                {
+                    name: "profile",
+                    disabled: button[0],
+                    navigate,
+                    setButton,
+                    buttonId: 0,
+                    path: `/user/${id}`
+                })}
+              {Button(
+                {
+                    name: "groups",
+                    disabled: button[1],
+                    navigate,
+                    setButton,
+                    buttonId: 1,
+                    path: `/`
+                    
+                })}
+                {Button(
+                {
+                    name: "users",
+                    disabled: button[2],
+                    navigate,
+                    setButton,
+                    buttonId: 2,
+                    path: `/`
+
+                })}
+                {Button(
+                {
+                    name: "notifications",
+                    disabled: button[3],
+                    navigate,
+                    setButton,
+                    buttonId: 3,
+                    path: `/`
+                })}
+
             </nav>
             <input
                 type="button"
@@ -33,5 +62,37 @@ export default function Navigation() {
                 }}
             />
         </header>
+        <Outlet/>
+        </>
+    )
+}
+
+interface buttonProps {
+    name: string, 
+    disabled: boolean, 
+    navigate: (str: string) => void, 
+    setButton: (arr: boolean[]) => void, 
+    buttonId: number,
+    path: string,
+}
+
+function Button({name, disabled, navigate, setButton, buttonId, path} : buttonProps) {
+    const buttonClass = disabled ? "button_disabled" : "button_active"
+
+
+    return(
+    <input
+        type="button"
+        className={"button navigation__button " + buttonClass}
+        value={name}
+        onClick={() => {
+            if (disabled) return
+            const newBtns = [false, false, false, false]
+            newBtns[buttonId] = true
+            setButton(newBtns)
+            navigate(path);
+            }
+        }
+        />
     )
 }
