83ac1c83dd51c70b3e2a75196d17a904a02a64f6
mercey123 <erik1telpt2@gmail.com>
2023-04-01 05:10:48 +0300

    SOCIAL-25 added changes for better code reusability: getFollowers renamed and can receive an endpoint string

--- client/src/components/followers-following/following-followers.tsx
+++ client/src/components/followers-following/following-followers.tsx
@@ -1,28 +1,27 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { getFollowers } from "../../additional-functions/getFollowers"
+import { getUsersList } from "../../additional-functions/getUsers"
 import { User } from "../models"
 import altAvatar from "../../assets/default-avatar.png"
 import "./followers-following.css"
-import { getFollowing } from "../../additional-functions/getFollowing"
 
 interface idProp {
     id: number
 }
 
 export default function FollowingFollowers({ id }: idProp) {
-    const [userList, setUserList] = useState<User[]>([])
+    const [followersList, setFollowersList] = useState<User[]>([])
     const [followingList, setFollowingList] = useState<User[]>([])
     const [res, setRes] = useState<boolean>(null)
     const navigate = useNavigate()
-    getFollowers({id, setUserList, setRes, navigate})
-    getFollowing({id, setFollowingList})
+    getUsersList({id, setUserList: setFollowersList, setRes, navigate, endpoint: "followers"})
+    getUsersList({id, setUserList: setFollowingList, setRes, navigate, endpoint: "followings"})
     return (
         <>
             {res === null ? null : res ? (
                 <div className="test-container">
                     <FollowingFollowersContainer header="Following" userList={followingList} />
-                    <FollowingFollowersContainer header="Followers" userList={userList} />
+                    <FollowingFollowersContainer header="Followers" userList={followersList} />
                 </div>
             ) : null}
         </>
