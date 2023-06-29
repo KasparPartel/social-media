5b394a6b7f2aed2f343948c3155b06349a3ac1e6
liki123 <ratsep10gmail.com>
2023-06-29 20:53:02 +0000

    join/leave requests

--- client/src/components/groups/fetch.ts
+++ client/src/components/groups/fetch.ts
@@ -1,72 +1,41 @@
 import fetchHandler from "../../additional-functions/fetchHandler"
 
+type joinLeaveResponse = {
+  data: {
+    joinStatus: number // 1 - not joined, 2 - requested, 3 - joined
+  }
+  errors: {
+    code: number,
+    description: string
+  }[]
+}
+
 export const sendJoinRequest = (
   groupId: number,
   setJoinStatus: React.Dispatch<React.SetStateAction<number>>,
 ) => {
-  fetchHandler(`http://localhost:8080/group/${groupId}/join`, "PUT").then((resp) => {
-    if (!resp.ok) {
-      throw new Error(`HTTP error: status ${resp.status}`)
+  fetchHandler(`http://localhost:8080/group/${groupId}/join`, "PUT").then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: status ${response.status}`)
     }
-    return resp.json()
-  }).then((resp) => { console.log(JSON.stringify(resp)) })
+    return response.json()
+  }).then((responseBody: joinLeaveResponse) => {
+    console.log(JSON.stringify(responseBody));
+    setJoinStatus(() => responseBody.data.joinStatus);
+  })
 }
 
-// import fetchHandler from "../../additional-functions/fetchHandler"
-// import { Dispatch, SetStateAction } from "react"
-// import { Post, PostComment } from "../models"
-
-// /**
-//  *  Fetches all post ids related to user
-//  */
-// export const getPostIds = (
-//     userId: number,
-//     setIdList: Dispatch<SetStateAction<number[]>>,
-//     setErr: Dispatch<SetStateAction<Error>>,
-// ) => {
-//     fetchHandler(`http://localhost:8080/user/${userId}/posts`, "GET")
-//         .then((res) => {
-//             if (!res.ok) {
-//                 throw new Error(`HTTP error: status ${res.status}`)
-//             }
-//             return res.json()
-//         })
-//         .then(
-//             (data) => setIdList(data.data),
-//             (err) => setErr(err),
-//         )
-// }
-
-// /**
-//  *  Fetches single post data
-//  */
-// export const getPostData = (
-//     postId: number,
-//     setPost: Dispatch<SetStateAction<Post>>,
-//     setErr: Dispatch<SetStateAction<Error>>,
-//     setIsLoading: Dispatch<SetStateAction<boolean>>,
-// ) => {
-//     fetchHandler(`http://localhost:8080/post/${postId}`, "GET")
-//         .then((res) => {
-//             if (!res.ok) {
-//                 throw new Error(`HTTP error: status ${res.status}`)
-//             }
-//             return res.json()
-//         })
-//         .then((data) => {
-//             fetchHandler(`http://localhost:8080/user/1`, `GET`)
-//                 .then((r) => r.json())
-//                 .then((r) => {
-//                     if (r.errors) throw new Error(`HTTP error: status ${r.status}`)
-//                     data.data.login = r.data.login
-//                     data.data.firstName = r.data.firstName
-//                     data.data.lastName = r.data.lastName
-//                     setPost(data.data)
-//                     setIsLoading(false)
-//                 })
-//         })
-//         .catch((err) => {
-//             setErr(err)
-//             setIsLoading(false)
-//         })
-// }
+export const sendLeaveRequest = (
+  groupId: number,
+  setJoinStatus: React.Dispatch<React.SetStateAction<number>>,
+) => {
+  fetchHandler(`http://localhost:8080/group/${groupId}/leave`, "PUT").then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: status ${response.status}`)
+    }
+    return response.json()
+  }).then((responseBody: joinLeaveResponse) => {
+    console.log(JSON.stringify(responseBody));
+    setJoinStatus(() => responseBody.data.joinStatus);
+  })
+}
